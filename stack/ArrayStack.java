87437bf4a517e7f3029e5fe7bccff93adfa7ad94
DongWon Kim <71078811+chamchi-god@users.noreply.github.com>
2022-03-05 23:51:19 +0900

    Add files via upload

---
+++ stack/ArrayStack.java
@@ -0,0 +1,57 @@
+package stack;
+
+public class ArrayStack<E> implements StackInterface<E>{
+    private E stack[];
+    private int topIndex;
+    private static final int DEFAULT_CAPACITY = 64;
+    private final E ERROR = null;
+
+    public ArrayStack(){
+        stack = (E[]) new Object[DEFAULT_CAPACITY];
+        topIndex = -1;
+    }
+
+    public ArrayStack(int n){
+        stack = (E[]) new Object[n];
+        topIndex = -1;
+    }
+
+    @Override
+    public void push(E newItem) {
+        if (isFull()){
+
+        }else{
+            stack[++topIndex] = newItem;
+        }
+    }
+
+    @Override
+    public E pop() {
+        if (isFull()){
+            return ERROR;
+        } else {
+           return stack[topIndex--];
+        }
+    }
+
+    @Override
+    public E top() {
+        if (isEmpty()) {
+            return ERROR;
+        } else return stack[topIndex];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (topIndex<0);
+    }
+    public boolean isFull() {
+        return (topIndex == stack.length-1);
+    }
+
+    @Override
+    public void popAll() {
+        stack = (E[]) new Object[stack.length];
+        topIndex = -1;
+    }
+}
